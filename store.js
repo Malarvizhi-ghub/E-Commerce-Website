01c9d592456239233872b9b4b0f0cb1b196235f3
Malarvizhi S <malarv625@gmail.com>
2026-06-19 10:29:54 +0530

    Add files via upload

---
+++ store.js
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from '../features/cartSlice.js';
+import productReducer from '../features/productSlice.js';
+
+
+export const store = configureStore({
+  reducer: {
+    cart: cartReducer,
+    products: productReducer,
+  },
+});
